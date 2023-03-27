b0f05d2957dac6f3a20409cfc97f7a18b0573bfd
seo-minjeong <smj12938@gmail.com>
2023-03-28 00:11:45 +0900

    컴포넌트 분리

--- udemy-study/src/App.js
+++ udemy-study/src/App.js
@@ -27,13 +27,14 @@ function App() {
       date: new Date(2022, 2, 28),
     },
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        // date={expenses[0].date}
+        date={expenses[0].date}
       />
     </div>
   );
